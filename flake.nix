6b7d586ca4adaf6f2f8a885512e15cb3f9010c2e
Mathijs <79464596+CouldBeMathijs@users.noreply.github.com>
2025-09-30 11:35:38 +0200

    Added .clang-format and flake.nix

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "A C++ project with SFML using Nix flakes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+
+      compiler = pkgs.clang;
+      # Use the specific SFML 2.6.2 package
+      sfml_2 = pkgs.sfml_2;
+      sfml-pkgs = with pkgs; [ compiler cmake gdb sfml_2 ];
+
+      cpp-env = pkgs.mkShell {
+        buildInputs = sfml-pkgs;
+        shellHook = ''
+          export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ sfml_2 ]}:$LD_LIBRARY_PATH
+          export CMAKE_PREFIX_PATH=${sfml_2}:$CMAKE_PREFIX_PATH
+
+          # Manual fix for SFML 2.6 CMake targets
+          export SFML_DIR=${sfml_2}/lib/cmake/SFML
+          
+          echo "Entering C++ development environment."
+          echo "Run 'cmake .' and 'make' to build your project."
+        '';
+      };
+
+      clion-env = pkgs.mkShell {
+        buildInputs = sfml-pkgs;
+        shellHook = ''
+          export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ sfml_2 ]}:$LD_LIBRARY_PATH
+          export CMAKE_PREFIX_PATH=${sfml_2}:$CMAKE_PREFIX_PATH
+
+          # Manual fix for SFML 2.6 CMake targets
+          export SFML_DIR=${sfml_2}/lib/cmake/SFML
+
+          echo "Launching CLion from host system..."
+          clion .
+        '';
+      };
+
+    in {
+      devShells.${system} = {
+        default = cpp-env;
+        clion = clion-env;
+      };
+    };
+}
