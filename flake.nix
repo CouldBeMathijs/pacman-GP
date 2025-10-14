9cbdd16ffa54e7adc21d794c8856af510fe47120
Mathijs <79464596+CouldBeMathijs@users.noreply.github.com>
2025-10-14 19:00:09 +0200

    Made platform independant

--- flake.nix
+++ flake.nix
@@ -1,55 +1,36 @@
 {
-  description = "A C++ project with SFML using Nix flakes";
+        description = "A C++ project";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-  };
+        inputs = {
+                # Get the systems from the flake's utility functions
+                nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+                flake-utils.url = "github:numtide/flake-utils";
+        };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
+        outputs = { self, nixpkgs, flake-utils }:
+                # Use flake-utils to iterate over common systems
+                flake-utils.lib.eachDefaultSystem 
+                (system: # <--- system variable is now passed in
+                        let
+                                pkgs = import nixpkgs {
+                                        inherit system;
+                                        config.allowUnfree = true;
+                                };
 
-      compiler = pkgs.clang;
-      # Use the specific SFML 2.6.2 package
-      sfml_2 = pkgs.sfml_2;
-      sfml-pkgs = with pkgs; [ compiler cmake gdb sfml_2 ];
+                                compiler = pkgs.clang;
+                                sfml-pkgs = with pkgs; [ compiler cmake valgrind doxygen ];
 
-      cpp-env = pkgs.mkShell {
-        buildInputs = sfml-pkgs;
-        shellHook = ''
-          export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ sfml_2 ]}:$LD_LIBRARY_PATH
-          export CMAKE_PREFIX_PATH=${sfml_2}:$CMAKE_PREFIX_PATH
-
-          # Manual fix for SFML 2.6 CMake targets
-          export SFML_DIR=${sfml_2}/lib/cmake/SFML
-          
-          echo "Entering C++ development environment."
-          echo "Please run your IDE from within this env to make sure all dependencies are present."
-        '';
-      };
-
-      clion-env = pkgs.mkShell {
-        buildInputs = sfml-pkgs;
-        shellHook = ''
-          export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ sfml_2 ]}:$LD_LIBRARY_PATH
-          export CMAKE_PREFIX_PATH=${sfml_2}:$CMAKE_PREFIX_PATH
-
-          # Manual fix for SFML 2.6 CMake targets
-          export SFML_DIR=${sfml_2}/lib/cmake/SFML
-
-          echo "Launching CLion from host system..."
-          clion .
-        '';
-      };
-
-    in {
-      devShells.${system} = {
-        default = cpp-env;
-        clion = clion-env;
-      };
-    };
+                                cpp-env = pkgs.mkShell {
+                                        buildInputs = sfml-pkgs;
+                                        shellHook = ''
+                                        echo "Entering C++ development environment for ${system}."
+                                        echo "Please launch your IDE from within this terminal to make sure all dependencies are met"
+                                        '';
+                                };
+                        in {
+                                devShells = {
+                                        default = cpp-env;
+                                };
+                        }
+                );
 }
