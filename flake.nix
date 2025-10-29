f4e35d64886b8f9dc4e7748d878d0ad3535a3a59
Mathijs <79464596+CouldBeMathijs@users.noreply.github.com>
2025-10-29 09:58:33 +0100

    Switched channels and installed sfml

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
         inputs = {
                 # Get the systems from the flake's utility functions
-                nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+                nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
                 flake-utils.url = "github:numtide/flake-utils";
         };
 
@@ -18,13 +18,22 @@
                                 };
 
                                 compiler = pkgs.clang;
-                                sfml-pkgs = with pkgs; [ compiler cmake valgrind doxygen ];
+
+                                # Use the specific SFML 2.6.2 package
+                                sfml_2 = pkgs.sfml_2;
+                                sfml-pkgs = with pkgs; [ compiler cmake gdb sfml_2 valgrind];
 
                                 cpp-env = pkgs.mkShell {
                                         buildInputs = sfml-pkgs;
                                         shellHook = ''
-                                        echo "Entering C++ development environment for ${system}."
-                                        echo "Please launch your IDE from within this terminal to make sure all dependencies are met"
+          export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ sfml_2 ]}:$LD_LIBRARY_PATH
+          export CMAKE_PREFIX_PATH=${sfml_2}:$CMAKE_PREFIX_PATH
+
+          # Manual fix for SFML 2.6 CMake targets
+          export SFML_DIR=${sfml_2}/lib/cmake/SFML
+
+          echo "Entering C++ development environment."
+          echo "Please run your IDE from within this env to make sure all dependencies are present."
                                         '';
                                 };
                         in {
