78cf9703e09c7b9d919b429c6071dc88735e267c
Mathijs <79464596+CouldBeMathijs@users.noreply.github.com>
2025-10-31 13:06:09 +0100

    Drawing of SFML-window

--- Representation/Game.cpp
+++ Representation/Game.cpp
@@ -2,5 +2,26 @@
 
 #include "Stopwatch.h"
 #include "World.h"
+#include <SFML/Graphics.hpp>
 void Game::run() {
+    sf::RenderWindow window(
+        sf::VideoMode(800, 600),
+        "Pacman AP",
+        sf::Style::Close | sf::Style::Resize
+    );
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+
+        // window.draw(myShape);
+
+        window.display();
+    }
 }
