a4a079db44399bb5af2ff9094f0a6262a5e42f51
Mathijs <79464596+CouldBeMathijs@users.noreply.github.com>
2025-11-04 10:40:32 +0100

    Started work on factories

--- logic/AbstractEntityFactory.h
+++ logic/AbstractEntityFactory.h
@@ -1,7 +1,19 @@
 
 #ifndef PACMAN_ABSTRACTENTITYFACTORY_H
 #define PACMAN_ABSTRACTENTITYFACTORY_H
+#include "entity_types/Ghost.h"
+#include "entity_types/Pacman.h"
+#include "entity_types/Wall.h"
 
-class AbstractEntityFactory {};
+class AbstractEntityFactory {
+protected:
+    AbstractEntityFactory() = default;
+
+public:
+    Pacman createPacman();
+    Ghost createGhost();
+    Wall createWall();
+
+};
 
 #endif // PACMAN_ABSTRACTENTITYFACTORY_H
