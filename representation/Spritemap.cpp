1f9bc39bfc31c52542eb90a37529aec049d5d824
Mathijs <79464596+CouldBeMathijs@users.noreply.github.com>
2025-11-18 13:17:59 +0100

    Ubuntu 24.04 did not like this not being there

--- representation/Spritemap.cpp
+++ representation/Spritemap.cpp
@@ -6,17 +6,7 @@ sf::Texture texture;
 bool initialized = false;
 
 using namespace Spritemap;
-const std::array<SpriteInfo, to_underlying(Count)> spriteDefinitions = {
 
-    SpriteInfo{410, 358, 6, 6},    // SpriteDefinition::Coin
-    SpriteInfo{601, 3, 35, 35},    // SpriteDefinition::Fruit
-    SpriteInfo{101, 4, 35, 35},    // SpriteDefinition::GhostBlue
-    SpriteInfo{151, 4, 35, 35},    // SpriteDefinition::GhostOrange
-    SpriteInfo{1, 554,35, 35},     // SpriteDefinition::GhostPanic
-    SpriteInfo{51, 4, 35, 35},     // SpriteDefinition::GhostPink
-    SpriteInfo{1, 4, 35, 35},      // SpriteDefinition::GhostRed
-    SpriteInfo{853, 5, 33, 33}     // SpriteDefinition::Pacman
-};
 } // namespace
 
 
--- representation/Spritemap.h
+++ representation/Spritemap.h
@@ -23,6 +23,17 @@ constexpr size_t to_underlying(const SpriteDefinition e) noexcept {
     return static_cast<size_t>(e);
 }
 
+const std::array<SpriteInfo, to_underlying(Count)> spriteDefinitions = {
+    SpriteInfo{410, 358, 6, 6},    // SpriteDefinition::Coin
+    SpriteInfo{601, 3, 35, 35},    // SpriteDefinition::Fruit
+    SpriteInfo{101, 4, 35, 35},    // SpriteDefinition::GhostBlue
+    SpriteInfo{151, 4, 35, 35},    // SpriteDefinition::GhostOrange
+    SpriteInfo{1, 554,35, 35},     // SpriteDefinition::GhostPanic
+    SpriteInfo{51, 4, 35, 35},     // SpriteDefinition::GhostPink
+    SpriteInfo{1, 4, 35, 35},      // SpriteDefinition::GhostRed
+    SpriteInfo{853, 5, 33, 33}     // SpriteDefinition::Pacman
+};
+
 SpriteInfo getSpriteInfo(SpriteDefinition);
 
 const sf::Texture& getTexture();
