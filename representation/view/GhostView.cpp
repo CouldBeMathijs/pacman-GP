a1a05486f057d3949ad041a9b7235b990e781dd6
Mathijs <79464596+CouldBeMathijs@users.noreply.github.com>
2025-12-05 13:45:07 +0100

    Introduced IGhostView

--- representation/view/GhostView.cpp
+++ representation/view/GhostView.cpp
@@ -1,13 +1,15 @@
 #include "GhostView.h"
 
+IGhostView::IGhostView(Assets::TextureLocation t, std::shared_ptr<IEntityModel> e) : IDirectionalEntityView(t, std::move(e), 2) {}
+
 BlueGhostView::BlueGhostView(std::shared_ptr<IEntityModel> e)
-    : IDirectionalEntityView(Assets::getSpriteInfo(Assets::GhostBlueBase), std::move(e), 2) {}
+    : IGhostView(Assets::getSpriteInfo(Assets::GhostBlueBase), std::move(e)) {}
 
 PinkGhostView::PinkGhostView(std::shared_ptr<IEntityModel> e)
-    : IDirectionalEntityView(Assets::getSpriteInfo(Assets::GhostPinkBase), std::move(e), 2) {}
+    : IGhostView(Assets::getSpriteInfo(Assets::GhostPinkBase), std::move(e)) {}
 
 OrangeGhostView::OrangeGhostView(std::shared_ptr<IEntityModel> e)
-    : IDirectionalEntityView(Assets::getSpriteInfo(Assets::GhostOrangeBase), std::move(e), 2) {}
+    : IGhostView(Assets::getSpriteInfo(Assets::GhostOrangeBase), std::move(e)) {}
 
 RedGhostView::RedGhostView(std::shared_ptr<IEntityModel> e)
-    : IDirectionalEntityView(Assets::getSpriteInfo(Assets::GhostRedBase), std::move(e), 2) {}
+    : IGhostView(Assets::getSpriteInfo(Assets::GhostRedBase), std::move(e)) {}
--- representation/view/GhostView.h
+++ representation/view/GhostView.h
@@ -2,22 +2,27 @@
 #define PACMAN_GHOST_VIEW_H
 #include "IDirectionalEntityView.h"
 
-class BlueGhostView final : public IDirectionalEntityView {
+class IGhostView : public IDirectionalEntityView {
+public:
+    IGhostView(Assets::TextureLocation t, std::shared_ptr<IEntityModel> e);
+};
+
+class BlueGhostView final : public IGhostView {
 public:
     explicit BlueGhostView(std::shared_ptr<IEntityModel>);
 };
 
-class OrangeGhostView final : public IDirectionalEntityView {
+class OrangeGhostView final : public IGhostView {
 public:
     explicit OrangeGhostView(std::shared_ptr<IEntityModel>);
 };
 
-class PinkGhostView final : public IDirectionalEntityView {
+class PinkGhostView final : public IGhostView{
 public:
     explicit PinkGhostView(std::shared_ptr<IEntityModel>);
 };
 
-class RedGhostView final : public IDirectionalEntityView {
+class RedGhostView final : public IGhostView {
 public:
     explicit RedGhostView(std::shared_ptr<IEntityModel>);
 };
