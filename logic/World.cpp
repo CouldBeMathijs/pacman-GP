b81da2d728caa773df72d6cdb3332b6a3501258d
Mathijs <79464596+CouldBeMathijs@users.noreply.github.com>
2025-12-14 16:07:41 +0100

    Removed debug prints and fixed ghosts getting stuck when they shouldn't TODO: isOnTileCenter-functie

--- logic/World.cpp
+++ logic/World.cpp
@@ -83,24 +83,14 @@ void World::update(const Direction::Cardinal d) {
 }
 
 std::vector<Direction::Cardinal> World::possibleDirections(const std::shared_ptr<IGhost>& ghost) {
-    std::string ghostColor;
-    if (std::dynamic_pointer_cast<BlueGhost>(ghost)) {
-        ghostColor = "Blue";
-    } else if (std::dynamic_pointer_cast<OrangeGhost>(ghost)) {
-        ghostColor = "Orange";
-    } else if (std::dynamic_pointer_cast<PinkGhost>(ghost)) {
-        ghostColor = "Pink";
-    } else {
-        ghostColor = "Red";
-    }
     const double distance = ghost->getSpeed() * Stopwatch::getInstance().getDeltaTime() * 2;
-    const auto hitBox = ghost->getHitBox().scaledBy(0.90);
+    const auto hitBox = ghost->getHitBox().scaledBy(0.9);
 
     // Try four primary directions
     std::map<Direction::Cardinal, Rectangle> test = {{Direction::Cardinal::EAST, hitBox.movedBy(distance, 0)},
                                                      {Direction::Cardinal::WEST, hitBox.movedBy(-distance, 0)},
-                                                     {Direction::Cardinal::NORTH, hitBox.movedBy(0, distance)},
-                                                     {Direction::Cardinal::SOUTH, hitBox.movedBy(0, -distance)}};
+                                                     {Direction::Cardinal::NORTH, hitBox.movedBy(0, -distance)},
+                                                     {Direction::Cardinal::SOUTH, hitBox.movedBy(0, distance)}};
 
     std::vector<Direction::Cardinal> out;
 
@@ -120,18 +110,12 @@ std::vector<Direction::Cardinal> World::possibleDirections(const std::shared_ptr
         std::erase(out, opposite);
     }
 
-    std::cout << ghostColor << ": ";
-    for (const auto dir : out) {
-        std::cout << Direction::to_string(dir) << " ";
-    }
-    std::cout << std::endl;
-    
     return out;
 }
 
 void World::updateGhosts(const Direction::Cardinal d) {
     for (const auto& ghost : m_ghosts) {
-        ghost->snapToGrid();
+        // ghost->snapToGrid();
         switch (ghost->getMode()) {
         case GhostMode::CHASING: {
 
@@ -140,7 +124,7 @@ void World::updateGhosts(const Direction::Cardinal d) {
 
             // Define the lookahead distance (10 "steps")
             // This ensures we don't move unless the long-range path is clear
-            const double lookAheadDist = moveDist * 1.234567890;
+            const double lookAheadDist = moveDist * 2;
 
             // 1. MOVEMENT CHECK:
             // We check the long distance (lookAheadDist). If that is clear, we perform the short move (moveDist).
