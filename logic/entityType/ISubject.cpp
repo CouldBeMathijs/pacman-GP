4f42aacc9a3db977a09c4e5bc29e49c2695c826c
GitHub Action (clang-format) <action@github.com>
2025-12-19 14:59:42 +0000

    style: auto-format C++ code with clang-format-18

--- logic/entityType/ISubject.cpp
+++ logic/entityType/ISubject.cpp
@@ -9,7 +9,7 @@ void ISubject::update(Direction::Cardinal) { notify(); }
 void ISubject::addObserver(std::weak_ptr<IObserver> n) { m_observers.push_back(std::move(n)); }
 
 void ISubject::notify() {
-    for (auto it = m_observers.begin(); it != m_observers.end(); ) {
+    for (auto it = m_observers.begin(); it != m_observers.end();) {
         // Try to promote weak_ptr to shared_ptr
         if (const auto observer = it->lock()) {
             observer->update();
