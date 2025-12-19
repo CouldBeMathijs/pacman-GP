7ea0eb3d8e3a649c52f71feca73a774a4ce53b83
MogelijksMathijs <79464596+CouldBeMathijs@users.noreply.github.com>
2025-12-19 10:38:57 +0100

    Add iomanip include to MenuState.cpp

--- representation/state/MenuState.cpp
+++ representation/state/MenuState.cpp
@@ -3,6 +3,7 @@
 #include "../AssetManager.h"
 #include "../Camera.h"
 #include "LevelState.h"
+#include <iomanip>
 
 MenuState::MenuState() {
     const auto& window = Camera::getInstance().getWindow();
