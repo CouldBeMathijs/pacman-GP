a7d9a619a2404acbb26deef8e955a1e088fde2ec
Mathijs <79464596+CouldBeMathijs@users.noreply.github.com>
2025-12-23 16:26:06 +0100

    Update flake.nix to use meson

--- flake.nix
+++ flake.nix
@@ -7,102 +7,115 @@
         };
 
         outputs = { self, nixpkgs, flake-utils }:
-                flake-utils.lib.eachDefaultSystem
-                (system:
+                flake-utils.lib.eachDefaultSystem (system:
                         let
                                 pkgs = import nixpkgs {
                                         inherit system;
-                                        config.allowUnfree = true;
+                                        # config.allowUnfree = true;
                                 };
 
+                                # Explicitly choose the compiler
                                 compiler = pkgs.clang;
                                 sfml_2 = pkgs.sfml_2;
-                                valgrind = pkgs.valgrind;
 
-                                # Packages needed for development shell
-                                sfml-dev-pkgs = with pkgs; [ 
-                                        compiler 
-                                        cmake 
-                                        gdb 
-                                        sfml_2 
-                                        valgrind 
-                                        meson 
-                                        pkg-config 
+                                # Package list for the development shell
+                                sfml-dev-pkgs = with pkgs; [
+                                        compiler
+                                        cmake      # Provided as requested
+                                        meson
                                         ninja
+                                        pkg-config # Essential for Meson
+                                        gdb
+                                        sfml_2
+                                        valgrind
                                 ];
 
-                                cppGamePackage = pkgs.stdenv.mkDerivation {
+                                # Helper to create a derivation using Meson
+                                mkPacmanDerivation = { optimization, debug, version }: pkgs.stdenv.mkDerivation {
                                         pname = "pacman";
-                                        version = "0.1.0-release";
-
-                                        src = ./.; # Source code is in the current flake directory
-
-                                        buildInputs = [ sfml_2 ];
-                                        nativeBuildInputs = [ pkgs.cmake pkgs.clang ];
-
-                                        cmakeFlags = [
-                                                "-DCMAKE_BUILD_TYPE=Release"
-                                                "-DSFML_DIR=${sfml_2}/lib/cmake/SFML"
-                                        ];
-
-                                        installPhase = ''
-                                                echo "Running custom install phase..."
-                                                mkdir -p $out
-                                                cp pacman $out/
-                                                cp -r $src/assets $out/
-                                                echo "Executable copied to $out/"
-                                        '';
-                                };
-
-                                # --- NEW: Package for Debug Build (for Valgrind) ---
-                                cppGamePackageDebug = pkgs.stdenv.mkDerivation {
-                                        pname = "pacman";
-                                        version = "0.1.0-debug";
-
+                                        inherit version;
                                         src = ./.;
 
-                                        buildInputs = [ sfml_2 ];
-                                        # Use clang for nativeBuildInputs
-                                        nativeBuildInputs = [ pkgs.cmake pkgs.clang ];
-
-                                        cmakeFlags = [
-                                                # Build with Debug symbols for useful Valgrind output
-                                                "-DCMAKE_BUILD_TYPE=Debug" 
-                                                "-DSFML_DIR=${sfml_2}/lib/cmake/SFML"
+                                        nativeBuildInputs = with pkgs; [ 
+                                                meson 
+                                                ninja 
+                                                pkg-config 
+                                                compiler 
                                         ];
 
+                                        buildInputs = [ sfml_2 ];
+
+                                        # We set these instead of buildtype to avoid conflicts with Nix defaults
+                                        mesonFlags = [
+                                                "-Doptimization=${optimization}"
+                                                "-Ddebug=${if debug then "true" else "false"}"
+                                        ];
+
+                                        # Meson build happens in a subdirectory; we copy results to $out
                                         installPhase = ''
-                                                echo "Running custom install phase for DEBUG build..."
-                                                mkdir -p $out
-                                                cp pacman $out/
-                                                cp -r $src/assets $out/
-                                                echo "Debug executable copied to $out/"
+                                        runHook preInstall
+                                        echo "Installing Pacman to $out..."
+                                        mkdir -p $out
+
+                                        # Copy the binary (assumed to be named 'pacman' in meson.build)
+                                        # We use a find command to be safe regardless of where Meson puts it
+                                        find . -name pacman -type f -executable -exec cp {} $out/ \;
+
+                                        # Copy assets
+                                        if [ -d "$src/assets" ]; then
+                                          cp -r $src/assets $out/
+                                        fi
+
+                                        echo "Installation complete."
+                                        runHook postInstall
                                         '';
                                 };
 
-                                cpp-env = pkgs.mkShell {
-                                        buildInputs = sfml-dev-pkgs;
-                                        shellHook = ''
-                                                export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ sfml_2 ]}:$LD_LIBRARY_PATH
-                                                export CMAKE_PREFIX_PATH=${sfml_2}:$CMAKE_PREFIX_PATH
-                                                export SFML_DIR=${sfml_2}/lib/cmake/SFML
-
-                                                echo "Entering C++ development environment."
-                                                echo "Run 'cmake . && make' to build, or 'nix build' outside this shell."
-                                                echo "Please run your IDE from within this env to make sure all dependencies are present."
-                                        '';
+                                # Define the two main package variants
+                                cppGamePackage = mkPacmanDerivation { 
+                                        optimization = "3"; 
+                                        debug = false;
+                                        version = "0.1.0-release"; 
                                 };
+
+                                cppGamePackageDebug = mkPacmanDerivation { 
+                                        optimization = "0"; 
+                                        debug = true;
+                                        version = "0.1.0-debug"; 
+                                };
+
                         in {
+                                # 'nix build'
                                 packages = {
                                         default = cppGamePackage;
-                                        sfml-cpp-project = cppGamePackage;
+                                        pacman-release = cppGamePackage;
                                         pacman-debug = cppGamePackageDebug;
                                 };
 
-                                devShells = {
-                                        default = cpp-env;
+                                # 'nix develop'
+                                devShells.default = pkgs.mkShell {
+                                        buildInputs = sfml-dev-pkgs;
+
+                                        shellHook = ''
+                                        export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ sfml_2 ]}:$LD_LIBRARY_PATH
+
+                                        # Environment variables to keep CMake happy within the dev shell
+                                        export CMAKE_PREFIX_PATH=${sfml_2}:$CMAKE_PREFIX_PATH
+                                        export SFML_DIR=${sfml_2}/lib/cmake/SFML
+
+                                        echo "--------------------------------------------------------"
+                                        echo "Pacman Dev Environment (Meson + CMake + Clang)"
+                                        echo "--------------------------------------------------------"
+                                        echo "To build with Meson:"
+                                        echo "  meson setup build && meson compile -C build"
+                                        echo ""
+                                        echo "To build with CMake:"
+                                        echo "  cmake -B build-cmake . && cmake --build build-cmake"
+                                        echo "--------------------------------------------------------"
+                                        '';
                                 };
 
+                                # 'nix run'
                                 apps.default = {
                                         type = "app";
                                         program = "${cppGamePackage}/pacman";
